0b68bb3e85bfc63fc4bb91288725cd00a25f8176
LiaCBA <moonlightt1083@gmail.com>
2025-01-19 15:09:31 +0400

    js

---
+++ script.js
@@ -0,0 +1,39 @@
+// ამოცანა 1
+let age = Number(prompt("რამდენი წლის ხარ?"));
+if (age < 12) {
+  alert("თქვენ არ გაქვთ შესვლის უფლება.");
+} else if (age >= 12) {
+  alert("თქვენ შეგიძლიათ დაესწროთ ფილმს.");
+}
+
+// ამოცანა 2
+// let color = prompt("რომელი ფერია შუქნიშანზე?");
+// if (color === "ყვითელი") {
+//   alert("მოემზადე");
+// } else if (color === "წითელი") {
+//   alert("გაჩერდი");
+// } else if (color === "მწვანე") {
+//   alert("შეგიძლია წახვიდე");
+// }
+
+// ამოცანა 3
+// let time = Number(prompt("რომელი საათია?"));
+// if (time >= 0 && time < 12) {
+//   alert("დილა მშვიდობის");
+// } else if (time >= 12 && time < 18) {
+//   alert("შუადღე მშვიდობის");
+// } else if (time >= 18 && time <= 23) {
+//   alert("საღამო მშვიდობის");
+// }
+
+// ამოცანა 4
+// let score = Number(prompt("ჩაწერეთ თქვენი ქულა"));
+// if (score >= 90 && score <= 100) {
+//   alert("A");
+// } else if (score >= 70 && score <= 89) {
+//   alert("B");
+// } else if (score >= 50 && score <= 69) {
+//   alert("C");
+// } else if (score < 50) {
+//   alert("F");
+// }
